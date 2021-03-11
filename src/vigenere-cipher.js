9b4e85b61d5bdcf3ff5031689e412804648acf84
alinalitvinka <alinalitvinko7@gmail.com>
2021-03-12 01:02:49 +0300

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -8,14 +8,58 @@ class VigenereCipheringMachine {
     if (message === undefined || key === undefined) {
       throw new Error('THROWN');
     } else {
-
+      if (key.length < message.length) {
+        for (let i=0; i<message.length; i++) {
+          key += key[i];
+        }
+      }
+      key = key.toUpperCase();
+      let mes = message.toUpperCase().match(/[a-z]/gi).join('');
+      let rez = '';
+      let n=0;
+      for (let i=0; i<message.length; i++) {     
+        if (message[i].match(/[a-z]/i) !== null ) {
+          let a = mes.charCodeAt(n)-65;
+          let b = key.charCodeAt(n)-65;
+          rez += String.fromCharCode(((a+b)%26)+65); 
+          n=n+1;
+        } else { 
+          rez += message[i];
+          }
+      } if (!this.machine) { 
+        return rez.split("").reverse().join("");
+      } else {
+        return rez;
+      }
     }
   }    
   decrypt(message, key) {
     if (message === undefined || key === undefined) {
       throw new Error('THROWN');
     } else {
-
+      if (key.length < message.length) {
+        for (let i=0; i<message.length; i++) {
+          key += key[i];
+        }
+      }
+      key = key.toUpperCase();
+      let mes = message.toUpperCase().match(/[a-z]/gi).join('');
+      let rez = '';
+      let n=0;
+      for (let i=0; i<message.length; i++) {     
+        if (message[i].match(/[a-z]/i) !== null ) {
+          let a = mes.charCodeAt(n)-65;
+          let b = key.charCodeAt(n)-65;
+          rez += String.fromCharCode(((a+26-b)%26)+65); 
+          n=n+1;
+        } else { 
+          rez += message[i];
+          }
+      } if (!this.machine) { 
+        return rez.split("").reverse().join("");
+      } else {
+        return rez;
+      }
     }
   }
 }
